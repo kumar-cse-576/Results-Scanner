d14395f08880db03b824657b0fb9146a5e30bd6d
kumar-cse-576 <kumar.cse.576@gmail.com>
2025-07-24 19:27:43 +0530

    initial commit

---
+++ src/results/Results.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package results;
+
+/**
+ *
+ * @author kumar
+ */
+public class Results {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       ResultsScrap v= new ResultsScrap();
+       v.setVisible(true);
+    }
+    
+}
